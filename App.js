a6c85cec9878e3cacac251b2e6f72f3b3e963e1b
Francisco Ortega <srfrancisco17@gmail.com>
2021-09-22 22:30:21 -0500

    Se realiza navegacion

--- App.js
+++ App.js
@@ -1,14 +1,64 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Card, ListItem, Input, Button, ThemeProvider } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-function HomeScreen() {
+
+function HomeScreen({ navigation }) {
+  return (
+    <ThemeProvider>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{ flex: 1,  flexDirection: "column" }} >
+          <Card>
+            <Card.Title>Clinica de Occidente S.A</Card.Title>
+              <Card.Image 
+                source={{
+                  uri:"https://www.eltiempo.com/files/article_main/uploads/2018/07/23/5b565c86b6cab.jpeg"
+                }}
+              />
+              <Card.Divider/>
+              <Text style={{marginBottom: 10}}>
+              Prestar servicios integrales de salud de mediana y alta complejidad con enfoque en las especialidades de Oncología y Cardiología.
+              </Text>
+              <Button
+                icon={<Icon name='code' color='#ffffff' />}
+                buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                title='Acceder' />
+          </Card>
+        </View>
+        <View style={{ flex: 1,  flexDirection: "column" }} >
+        <Card>
+          <Card.Title>Angiografia de Occidente</Card.Title>
+            <Card.Image 
+              source={{
+                uri:"https://www.hunterdouglas.cl/ap/img/cl/enews_imagenes/675x440-1/enews_imagenes_imagen_6323.JPG"
+              }}
+            />
+            <Card.Divider/>
+            <Text style={{marginBottom: 10}}>
+            Prestar servicios integrales de salud de mediana y alta complejidad con enfoque en las especialidades de Oncología y Cardiología.
+            </Text>
+            <Button
+              icon={<Icon name='code' color='#ffffff' />}
+              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+              onPress={() => navigation.navigate('Details')}
+              title='Acceder' />
+        </Card>
+      </View>
+      </View>
+    </ThemeProvider>
+  );
+}
+
+function DetailsScreen({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>Details Screen</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
@@ -20,6 +70,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
