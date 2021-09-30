85e9163e40f8be9b1364c3121c3b316e3678d8bc
sistemas <sistemas@COM54465.lan.cdo-sa.com>
2021-09-30 15:43:55 -0500

    Se modifica la interfaz de solicitud de citas

--- screens/SolicitudCitas.js
+++ screens/SolicitudCitas.js
@@ -1,10 +1,90 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, ScrollView, TouchableOpacity } from 'react-native';
+import { ListItem, Icon, SearchBar } from 'react-native-elements';
+
+const list = [
+  {
+    title: 'CIRUGIA DE TORAX',
+    icon: 'medkit'
+  },
+  {
+    title: 'CONSULTA POR REUMATOLOGIA',
+    icon: 'medkit'
+  },
+  {
+    title: 'CONSULTA POR OTORRINOLARINGOLOGIA',
+    icon: 'medkit'
+  },
+  {
+    title: 'CONSULTA POR CIRUGIA PLASTICA',
+    icon: 'medkit'
+  },
+  {
+    title: 'CONSULTA POR CIRUGIA PLASTICA MAXILO-FACIAL',
+    icon: 'medkit'
+  },
+  {
+    title: 'CONSULTA POR CIRUGIA VASCULAR',
+    icon: 'medkit'
+  },
+  {
+    title: 'CONSULTA POR INFECTOLOGIA',
+    icon: 'medkit'
+  },
+  {
+    title: 'CONSULTA POR MEDICINA GENERAL',
+    icon: 'medkit'
+  },
+  {
+    title: 'CONSULTA POR INTERNISTA NEUMOLOGO',
+    icon: 'medkit'
+  },
+  {
+    title: 'CONSULTA POR DERMATOLOGIA',
+    icon: 'medkit'
+  },
+  {
+    title: 'CONSULTA POR OFTALMOLOGIA',
+    icon: 'medkit'
+  },
+  {
+    title: 'CONSULTA POR ORTOPEDIA ONCOLOGIA',
+    icon: 'medkit'
+  },
+  {
+    title: 'CUIDADOS PALIATIVOS',
+    icon: 'medkit'
+  },
+]
 
 function SolicitudCitas(){
 	return(
 		<View>
-			<Text>hola</Text>
+      <SearchBar
+        placeholder="Type Here..."
+				lightTheme={true}
+      />
+			<ScrollView>
+			{
+			list.map((item, i) => (
+				<TouchableOpacity 
+					key={i}
+					onPress={ ()=> alert("hola") }
+				>
+					<ListItem bottomDivider>
+						<Icon
+							name={item.icon} 
+							type="font-awesome-5"
+						/>
+						<ListItem.Content>
+							<ListItem.Title>{item.title}</ListItem.Title>
+						</ListItem.Content>
+						<ListItem.Chevron />
+					</ListItem>
+				</TouchableOpacity>
+			))
+			}
+			</ScrollView>
 		</View>
 	);
 }
