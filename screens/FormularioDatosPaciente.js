6fe1f80ea377f04094eab09fae2463d2153eb0c1
Francisco Ortega <srfrancisco17@gmail.com>
2021-09-30 16:35:41 -0500

    Se incluye la libreria de react-native-datetimepicker

--- screens/FormularioDatosPaciente.js
+++ screens/FormularioDatosPaciente.js
@@ -1,14 +1,71 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, Button } from 'react-native';
 import { Icon, Input  } from 'react-native-elements';
+import { TextInput } from 'react-native-paper';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-function FormularioDatosPaciente(){
+// function FormularioDatosPaciente(){
+
+const FormularioDatosPaciente = () => {
+  const [text, setText] = React.useState('');
+
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);  
+
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };  
+
 	return(
 		<View>
-      <Input
-        label="DOCUMENTO"
-        placeholder=''
+      <TextInput
+        label="Primer Nombre"
+        value={text}
+        onChangeText={text => setText(text)}
+      /> 
+      <TextInput
+        label="Segundo Nombre"
+        value={text}
+        onChangeText={text => setText(text)}
       />
+      <TextInput
+        label="Primer Apellido"
+        value={text}
+        onChangeText={text => setText(text)}
+      />
+      <TextInput
+        label="Segundo Apellido"
+        value={text}
+        onChangeText={text => setText(text)}
+      />
+      <Button onPress={showDatepicker} title="Show date picker!" />
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode="date"
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}      
 		</View>
 	);
 }
